b16079f2615830830c69aaf9e6dd14af5554cbf8
MaryGuk <mkosevskaa@gmail.com>
2022-08-31 19:34:37 +0300

    beginn

---
+++ src/components/button/button.js
@@ -0,0 +1,19 @@
+import classNames from "classnames";
+import "./button.css";
+
+export const Button = ({ onClick, type, children, size = "s" }) => {
+  const btnClass = classNames({
+    "btn": true,
+    "btn--secondary": type === "secondary",
+    "btn--primary": type === "primary",
+    "btn--small": size === "s",
+    "btn--medium": size === "m",
+  });
+  return (
+    <div>
+      <button className={btnClass} onClick={onClick}>
+        {children}
+      </button>
+    </div>
+  );
+};
