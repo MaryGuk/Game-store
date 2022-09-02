0aeaf8bbd418f630454468fd3dda44b1d94b56c8
MaryGuk <mkosevskaa@gmail.com>
2022-09-02 15:29:20 +0300

    genre, span

--- src/components/game-buy/game-buy.js
+++ src/components/game-buy/game-buy.js
@@ -5,9 +5,11 @@ export const GameBuy = ({ game }) => {
   return (
     <div className="game-buy">
       <span className="game-buy__price">{game.price} руб. </span>
-      <Button type="primary" onClick={() => null}>
-        В Корзину
-      </Button>
+      <span className="game-buy__button">
+        <Button type="primary" onClick={() => null}>
+          В Корзину
+        </Button>
+      </span>
     </div>
   );
 };
