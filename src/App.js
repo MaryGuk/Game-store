ba649ef8c358d16b9b08570c59a88cd10975ed8f
MaryGuk <mkosevskaa@gmail.com>
2022-09-12 17:18:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import { HomePage } from "./pages/home-page";
 import { Header } from "./components/header";
+import { Provider } from "react-redux";
+import { store } from "./redux";
+
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- src/components/cart-block/cart-block.js
+++ src/components/cart-block/cart-block.js
@@ -1,11 +1,29 @@
 import "./cart-block.css";
 import { BiCartAlt } from "react-icons/bi";
+import { useSelector } from "react-redux/es/exports";
+import { calcTotalPrice } from "../utils";
+import { CartMenu } from "../cart-menu/cart-menu";
+import { useState } from "react";
+import { ItemsInCart } from "../items-in-cart/items-in-cart";
 
 export const CartBlock = () => {
+  const [isCartMenuVisible, setIsCartMenuVisible] = useState(false);
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const totalPrice = calcTotalPrice(items);
+
   return (
     <div className="cart-block">
-      <BiCartAlt size={25} className="cart-block__icon" />
-      <span className="cart-block__total-price"> 2313 руб. </span>
+      <ItemsInCart quantity={items.length} />
+      <BiCartAlt
+        color="white"
+        size={25}
+        className="cart-block__icon"
+        onClick={() => setIsCartMenuVisible(!isCartMenuVisible)}
+      />
+      {totalPrice > 0 ? (
+        <span className="cart-block__total-price"> {totalPrice} руб. </span>
+      ) : null}
+      {isCartMenuVisible && <CartMenu items={items} onClick={() => null} />}
     </div>
   );
 };
