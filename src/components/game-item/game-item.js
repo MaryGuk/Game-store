7e85ced1f50809c639837236ea5241325be006bb
MaryGuk <mkosevskaa@gmail.com>
2022-09-13 19:47:36 +0300

    game reducer

--- src/components/game-item/game-item.js
+++ src/components/game-item/game-item.js
@@ -1,11 +1,21 @@
-import "./game-item.css";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setCurrentGame } from "../../redux/games/reducer";
 import { GameCover } from "../game-cover";
 import { GameBuy } from "../game-buy";
 import { GameGenre } from "../game-genre";
+import "./game-item.css";
 
 export const GameItem = ({ game }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(setCurrentGame(game));
+    navigate.push(`/app/${game.title}`);
+  };
   return (
-    <div className="game-item">
+    <div className="game-item" onClick={handleClick}>
       <GameCover image={game.image} />
       <div className="game-item__details">
         <span className="game-item__title"> {game.title}</span>
